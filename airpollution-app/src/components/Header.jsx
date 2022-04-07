66c5bf6df4e4ec111ffb0924c254cf67c065f672
Zeynab Hasanli <zeynabhasanli@Zeynabs-MacBook-Pro.local>
2022-04-07 13:45:00 -0400

    added links to header

--- airpollution-app/src/components/Header.jsx
+++ airpollution-app/src/components/Header.jsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return(
         <header className="app-header">
             <h1 className='header-logo'>Logo here</h1>
-
-            <button className='header-button'>Home</button>
-
-            <button className='header-button'>Recommendations</button>
+            <nav className="header-nav">
+                <ul className="navigations">
+                    <li className="home-link" >
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="recomendations-link">
+                        <Link to="/recomendations">recomendations</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 
