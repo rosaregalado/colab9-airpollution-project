cf2de01f4a04b2b82d345f545d3636c53298b6d6
Zeynab Hasanli <zeynabhasanli@Zeynabs-MacBook-Pro.local>
2022-04-06 20:52:49 -0400

    stashing

--- airpollution-app/src/Main.jsx
+++ airpollution-app/src/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}></Route>
-      <Route path='/recomendations' element={Recomendations}></Route>
+      <Route path='/recomendations' element={<Recomendations/>}></Route>
     </Routes>
   );
 }
